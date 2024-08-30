8946128977cb2b832f7f50f4330f8eebd4f276bb
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-08-30 09:52:40 +0000

    [finish]応用課題4

---
+++ app/models/relationship.rb
@@ -0,0 +1,18 @@
+class Relationship < ApplicationRecord
+  
+  has_one_attached :profile_image
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+end
